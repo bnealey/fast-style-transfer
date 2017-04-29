519b70ea9eb171a9928c3db1ee3abac1f699334e
Brandon Nealey <bnealey@outlook.com>
2017-04-29 14:48:13 -0400

    Added another example picture of a Purple Acid Scorpion

--- the-code/evaluate_all.bat
+++ the-code/evaluate_all.bat
@@ -1,11 +1,11 @@
-python evaluate.py --checkpoint .\ckpts\udnie.ckpt --in-path ..\examples\input\chateauneuf.jpg --out-path ..\examples\output\chateauneuf-udnie.jpg
+python evaluate.py --checkpoint .\ckpts\udnie.ckpt --in-path ..\examples\input\PAASaKaoms.png --out-path ..\examples\output\PAASaKaoms-udnie.png
 
-python evaluate.py --checkpoint .\ckpts\la_muse.ckpt --in-path ..\examples\input\chateauneuf.jpg --out-path ..\examples\output\chateauneuf-la_muse.jpg
+python evaluate.py --checkpoint .\ckpts\la_muse.ckpt --in-path ..\examples\input\PAASaKaoms.png --out-path ..\examples\output\PAASaKaoms-la_muse.png
 
-python evaluate.py --checkpoint .\ckpts\scream.ckpt --in-path ..\examples\input\chateauneuf.jpg --out-path ..\examples\output\chateauneuf-scream.jpg
+python evaluate.py --checkpoint .\ckpts\scream.ckpt --in-path ..\examples\input\PAASaKaoms.png --out-path ..\examples\output\PAASaKaoms-scream.png
 
-python evaluate.py --checkpoint .\ckpts\wave.ckpt --in-path ..\examples\input\chateauneuf.jpg --out-path ..\examples\output\chateauneuf-wave.jpg
+python evaluate.py --checkpoint .\ckpts\wave.ckpt --in-path ..\examples\input\PAASaKaoms.png --out-path ..\examples\output\PAASaKaoms-wave.png
 
-python evaluate.py --checkpoint .\ckpts\rain_princess.ckpt --in-path ..\examples\input\chateauneuf.jpg --out-path ..\examples\output\chateauneuf-rain_princess.jpg
+python evaluate.py --checkpoint .\ckpts\rain_princess.ckpt --in-path ..\examples\input\PAASaKaoms.png --out-path ..\examples\output\PAASaKaoms-rain_princess.png
 
-python evaluate.py --checkpoint .\ckpts\wreck.ckpt --in-path ..\examples\input\chateauneuf.jpg --out-path ..\examples\output\chateauneuf-wreck.jpg
+python evaluate.py --checkpoint .\ckpts\wreck.ckpt --in-path ..\examples\input\PAASaKaoms.png --out-path ..\examples\output\PAASaKaoms-wreck.png
